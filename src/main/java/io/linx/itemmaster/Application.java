df78a23b72fb4f4a5eb6066dd552d11749c2411f
Lingani Tshuma <Lingani.Tshuma@absa.co.za>
2016-10-05 16:33:45 +0200

    Added service discovery

--- src/main/java/io/linx/itemmaster/Application.java
+++ src/main/java/io/linx/itemmaster/Application.java
@@ -2,9 +2,11 @@ package io.linx.itemmaster;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
   public static void main(String[] args) {
