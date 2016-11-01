9e947c03bb0d5ab5941612e5d844b4de1b043a59
Lingani Tshuma <Lingani.Tshuma@absa.co.za>
2016-11-01 15:14:22 +0200

    Cleaned up after using this project for some POCs

--- src/main/java/io/linx/itemmaster/Application.java
+++ src/main/java/io/linx/itemmaster/Application.java
@@ -3,13 +3,11 @@ package io.linx.itemmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/io/linx/itemmaster/model/Item.java
+++ src/main/java/io/linx/itemmaster/model/Item.java
@@ -1,39 +1,43 @@
 package io.linx.itemmaster.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 @Entity
 public class Item {
-  @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
-  private Integer id;
-  @Column
-  private boolean checked;
-  @Column
-  private String description;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private boolean checked;
+    @Column
+    private String description;
 
-  public Integer getId() {
-    return id;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public boolean isChecked() {
-    return checked;
-  }
+    public boolean isChecked() {
+        return checked;
+    }
 
-  public void setChecked(boolean checked) {
-    this.checked = checked;
-  }
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
